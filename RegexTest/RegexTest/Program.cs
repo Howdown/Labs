7161a14d06d589e5190f99d25c88f66b950315cd
mike <knowes12@gmail.com>
2017-07-27 18:34:59 +0300

    RegexTest

---
+++ RegexTest/RegexTest/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RegexTest
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ВВедите численное выражение типа a + b = c");
+            var inputExpression = Console.ReadLine();
+            var regex = @"([-]{0,1})\d(\s{0,5})[*,-,+,/](\s{0,5})\d(\s{0,5})[=](\s{0,5}([-]{0,1}))\d";
+            if (inputExpression != null && Regex.IsMatch(inputExpression, regex))
+            {
+                Console.WriteLine("Выражение введено верно");
+            }
+            else
+            {
+                Console.WriteLine("Выражение введено неверно");
+            }
+        }
+    }
+}
